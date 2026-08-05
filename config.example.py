d9813e32293d39a4e94ff531e0b396b82372a03f
zrxss <pavlikovroma65@gmail.com>
2026-08-05 18:18:36 +0400

    transferring some structure to the config

--- config.example.py
+++ config.example.py
@@ -5,5 +5,9 @@ TG_PROXY = None  # Например: "http://login:password@host:port"
 SECRET = "u38f9ud93f2h9f"   # Используется для подписи запросов к tosamara API
 
 EXCLUDED_NEXT_STOP_IDS = set() # Фильтр который используется для исключения ошибочных прибытий возле конечных остановок.
-KR_ID = 'your_bus_id'
-KS_ID = 'your_stop_id'
+KR_ID = 49
+KS_ID = 27
+
+WALKING_TIME_SECONDS = 210
+MIN_WAIT_SECONDS = 180
+MAX_WAIT_SECONDS = 300
