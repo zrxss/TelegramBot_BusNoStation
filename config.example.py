fff08f4e0e5b7af286a87a256df7a26305418685
zrxss <pavlikovroma65@gmail.com>
2026-08-05 16:12:03 +0400

    add config example and guide аÑfor buses and stops

---
+++ config.example.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = 'your_bot_token_here'
+
+TG_PROXY = None  # Например: "http://login:password@host:port"
+
+SECRET = "u38f9ud93f2h9f"   # Используется для подписи запросов к tosamara API
+
+EXCLUDED_NEXT_STOP_IDS = set() # Фильтр который используется для исключения ошибочных прибытий возле конечных остановок.
+KR_ID = 'your_bus_id'
+KS_ID = 'your_stop_id'
